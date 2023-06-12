cbaa1f5089c5ffdfca59302e99b41f84292d9c72
Ricardo Ponce <ricardoponces@gmail.com>
2023-06-12 10:02:12 -0400

    tarea 2 actividad 4

---
+++ src/main/java/com/kibernumacademy/actividad4/Clase1.java
@@ -0,0 +1,10 @@
+package com.kibernumacademy.actividad4;
+
+public class Clase1 {
+
+	public static void main(String[] args) {
+		System.out.println("Hola a todos mis colegas del grupo 4");
+
+	}
+
+}
